b3b96b02489a9ae2a3b47d3b56bd4fb6e8e60054
mokshadamalpani <76507770+mokshadamalpani@users.noreply.github.com>
2022-01-11 15:56:46 +0530

    Update numbertoword.py

--- numbertoword.py
+++ numbertoword.py
@@ -1,12 +1,12 @@
 oneDigit=['','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
 twoDigit=['Ten','Eleven','Twelve','Thirteen','Fourteen','Fifteen','Sixteen','Seventeen','Eighteen','Nineteen']
 tensDigit=['Twenty','Thirty','Forty','Fifty','Sixty','Seventy','Eighty','Ninety']
-suffix=['Thousand','Million','Billion','Trillion','Quadrillion','Quintillion','sextillion']
+suffix=['Thousand','Million','Billion','Trillion','Quadrillion','Quintillion','sextillion','']
 def convert(str) :
     word=''
-    if len(str)==1 :
-        word=oneDigit[int(str)]
-    elif len(str)==2 :
+    if str[0]=='0' and str[1]=='0' and str[2]=='0':
+      return word
+    if len(str)==2 :
         if int(str)//10>=2 :
             word=tensDigit[(int(str)//10)-2]+oneDigit[int(str)%10]
         else :
@@ -15,34 +15,46 @@ def convert(str) :
             else :
                 word=twoDigit[int(str)%10]
     elif len(str)==3 :
+      if str[0]!='0':
         if int(str[1:])//10>=2 :
             word=oneDigit[int(str)//100]+' '+'Hundred'+' '+tensDigit[(int(str[1:])//10)-2]+' '+oneDigit[int(str[1:])%10]
         else :
-            if str[1]=='0':
+            if str[1]=='0' :
                 word=oneDigit[int(str)//100]+' '+'Hundred'+' '+oneDigit[int(str[1:])%10]
             else :
                 word=oneDigit[int(str)//100]+' '+'Hundred'+' '+twoDigit[int(str[1:])%10]
+      else :
+        if int(str[1:])//10>=2 :
+            word=oneDigit[int(str)//100]+' '+' '+tensDigit[(int(str[1:])//10)-2]+' '+oneDigit[int(str[1:])%10]
+        else :
+          if str[1]=='0':
+              word=oneDigit[int(str[1:])%100]
+          else :
+              word=twoDigit[int(str[1:])%10]    
     return word            
 
 wordf=''
 wordfd=''
 str=input("Enter Any Number :")
-e=len(str)%3
-j=(len(str)//3)-1
-if e==1 :
-    wordfd=wordfd+''+oneDigit[int(str[0])]+' '+suffix[j]+' '
-    j=j-1
-elif e==2 :
-    wordfd=wordfd+' '+convert(str[0:2])+' '+suffix[j]+' '
-    j=j-1
+div=len(str)%3
+pos=(len(str)//3)-1
+if div==1 :
+    wordfd=wordfd+''+oneDigit[int(str[0])]+' '+suffix[pos]+' '
+    pos=pos-1
+elif div==2 :
+    wordfd=wordfd+' '+convert(str[0:2])+' '+suffix[pos]+' '
+    pos=pos-1
 
 j=(len(str)//3)-2
-for i in range(e,len(str),3):
+for i in range(div,len(str),3):
     wordf=convert(str[i:i+3])
-    if i==len(str)-3 :
-        wordfd=wordfd+wordf+' '
+    if wordf=='':
+      j=j-1
     else:
-        wordfd=wordfd+wordf+' '+suffix[j]+' '
-    j=j-1
+      if i==len(str)-3 :
+          wordfd=wordfd+wordf+' '
+      else:
+          wordfd=wordfd+wordf+' '+suffix[j]+' '
+      j=j-1
 
 print(wordfd)    
