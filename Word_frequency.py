5ef0e6b50f7cae184ed0105f2e2f7ef2d2796ede
mokshadamalpani <76507770+mokshadamalpani@users.noreply.github.com>
2022-01-14 13:31:04 +0530

    Update Word_frequency.py

--- Word_frequency.py
+++ Word_frequency.py
@@ -9,14 +9,25 @@ def count(word):
 sentence=input("Enter data you want")
 dic={}
 list=sentence.split()
-
 for word in list:
     count(word)
-    
-for Keys in dic:
-    print("Frequency of ",Keys,end="")
-    print(":",end="")
-    print(dic[Keys],end="")
-    print()
-    
 
+while(1):
+    key=input("Enter key which you want to search the occurences")
+    if key=="stop":
+        break;
+    else:
+        for Keys in dic:
+            if Keys==key:
+                flag=1
+                print("Frequency of ",Keys,":",end="")
+                print(dic[Keys],end="")
+                print()
+                break
+            else:
+                flag=0
+
+        if flag==0:
+            print("Key Not Found")
+
+    
